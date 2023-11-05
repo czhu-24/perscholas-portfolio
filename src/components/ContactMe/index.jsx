c298972b45d10e4ce1d91d101d656e3f5e3b00ad
czhu-24 <blue.ink.stains@gmail.com>
2023-11-04 21:50:11 -0700

    made changes to ContactMe, used useRef instead of useState

--- src/components/ContactMe/index.jsx
+++ src/components/ContactMe/index.jsx
@@ -1,31 +1,25 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs from 'emailjs-com';
-
 
 const ContactMe = () => {
   const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
   const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
   const PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+  // with useState, this component is re-rendering on each user input
+  // but with useRef, it's persisting even after user input
+  const form = useRef();
 
-  const handleSubmit = (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(SERVICE_ID, TEMPLATE_ID, e.target, PUBLIC_KEY)
+      .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
       .then(
         (result) => {
           console.log('Email sent successfully:', result.text);
-          setFormData({
-            name: '',
-            email: '',
-            message: '',
-          });
+          // clear the form
+          form.current.reset();
         },
         (error) => {
           console.error('Email sending failed:', error);
@@ -33,31 +27,25 @@ const ContactMe = () => {
       );
   };
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+
 
   return (
     <div id="contactme" className="container">
       <h2 className="text-center">Contact Me</h2>
       <div className="row justify-content-center">
         <div className="col-lg-8" style={{ maxWidth: '70%' }}>
-          <form className="mb-3" onSubmit={handleSubmit}>
+          <form ref={form} className="mb-3" onSubmit={sendEmail}>
             <div className="mb-3">
               <label htmlFor="name" className="form-label">Name:</label>
-              <input type="text" name="name" id="name" className="form-control" onChange={handleChange} value={formData.name} />
+              <input type="text" name="from_name" id="name" className="form-control" />
             </div>
             <div className="mb-3">
               <label htmlFor="email" className="form-label">Email:</label>
-              <input type="email" name="email" id="email" className="form-control" onChange={handleChange} value={formData.email} />
+              <input type="email" name="reply_to" id="email" className="form-control" />
             </div>
             <div className="mb-3">
               <label htmlFor="message" className="form-label">Message:</label>
-              <textarea name="message" id="message" rows="10" className="form-control" onChange={handleChange} value={formData.message} />
+              <textarea name="message" id="message" rows="10" className="form-control" />
             </div>
             <button type="submit" className="btn btn-primary">Submit</button>
           </form>
